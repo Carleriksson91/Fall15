eb12045be14738572bc2bb5db99acbff13118230
Carl Eriksson <carl.eriksson@funka.com>
2015-11-07 14:43:54 +0100

    Added Project to version control

---
+++ Fall15/Fall15/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fall15.MvcApplication" Language="C#" %>
---
+++ Fall15/Fall15/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fall15.Startup))]
+namespace Fall15
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
